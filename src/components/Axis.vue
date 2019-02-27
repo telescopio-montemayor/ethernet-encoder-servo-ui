aee0caa579c4b438b1ef3a2f1584c55859687de7
Adrián Pardini <github@tangopardo.com.ar>
2019-02-27 08:37:18 -0300

    Axis: simplify interface, toggle to edit

--- src/components/Axis.vue
+++ src/components/Axis.vue
@@ -1,8 +1,8 @@
 <template>
-  <b-container fluid :class="{'offline border border-danger rounded': !$encoder.online}">
+  <b-container fluid :class="{'offline border border-danger rounded': !$encoder.online}" class="mb-5">
     <h4 class="text-sm-left"> {{ axis.state.name }} <small> <span v-show="!$encoder.online" class="badge xbadge-pill badge-danger"> offline </span></small> </h4>
 
-    <b-row align-content="start" align-h="start" class="text-left">
+    <b-row align-content="start" align-h="start" align-v="center" class="text-left align-items-sm-center my-3">
       <b-col sm="auto" v-if="axis.state.supports_hour_angle">
         <span> Angle Units: </span>
         <b-form-radio-group
@@ -48,21 +48,30 @@
       </b-col>
     </b-row>
 
-    <b-row align-content="start" align-h="start" class="text-left">
+    <b-row align-content="start" align-h="start" align-v="center" class="text-left align-items-sm-center my-3">
 
       <b-col sm="auto">
         <span> Current position </span> <AngleDisplay :angle="displayDMS ? axis.state.position_angle : axis.state.position_astronomical "/>
       </b-col>
 
       <b-col sm="auto">
-        <span> Target </span> <AngleDisplay :angle="displayDMS ? axis.state.target_angle : axis.state.target_astronomical "/>
-        <AngleEdit v-bind:angle="newTarget" @update="goto($event)"/>
+        <span> Target </span>
+        <AngleDisplay :angle="displayDMS ? axis.state.target_angle : axis.state.target_astronomical" v-b-toggle="`target-${ axis.$id }`"/>
+        <b-button size="sm" variant="outline-primary" v-b-toggle="`target-${ axis.$id }`">
+          <font-awesome-icon icon="edit" />
+        </b-button>
+      </b-col>
+
+      <b-col sm="auto">
+        <b-collapse :id="`target-${ axis.$id }`" size="sm">
+            <AngleEdit v-bind:angle="newTarget" @update="goto($event); $root.$emit('bv::toggle::collapse', `target-${ axis.$id }`)"/>
+        </b-collapse>
       </b-col>
 
     </b-row>
 
 
-    <b-row align-content="start" align-h="start" class="text-left">
+    <b-row align-content="start" align-h="start" align-v="center" class="text-left align-items-sm-center my-3">
       <b-col sm="auto">
         <span> Nudge </span>
         <b-button size="sm" variant="outline-primary" @click="nudge('down')">
@@ -71,10 +80,15 @@
         <b-button size="sm" variant="outline-primary" @click="nudge('up')">
           <font-awesome-icon icon="caret-up" />
         </b-button>
+        <b-button size="sm" variant="outline-primary" v-b-toggle="`nudge-${ axis.$id }`">
+          <font-awesome-icon icon="edit" />
+        </b-button>
       </b-col>
 
       <b-col sm="auto">
-        <AngleEdit v-bind:angle="nudgeAmount" @change="nudgeAmount = $event" no-update-button/>
+        <b-collapse :id="`nudge-${ axis.$id }`" size="sm">
+          <AngleEdit v-bind:angle="nudgeAmount" @change="nudgeAmount = $event" no-update-button/>
+        </b-collapse>
       </b-col>
     </b-row>
 
@@ -91,9 +105,10 @@
 
 <script>
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCaretUp, faCaretDown  } from '@fortawesome/free-solid-svg-icons'
+import { faCaretUp, faCaretDown, faEdit  } from '@fortawesome/free-solid-svg-icons'
 library.add(faCaretUp);
 library.add(faCaretDown);
+library.add(faEdit);
 
 import AngleDisplay from './AngleDisplay.vue'
 import AngleEdit from './AngleEdit.vue'
